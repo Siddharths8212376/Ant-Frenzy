126d2621b396b7a23d396ccd9e54ed61302fe4fb
Siddharths8212376 <40669232+Siddharths8212376@users.noreply.github.com>
2019-12-20 02:58:54 +0530

    Added the pop-up score change on ant kills...

--- Scripts/ScoreManager.cs
+++ Scripts/ScoreManager.cs
@@ -2,18 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System;
+
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public TextMeshProUGUI text;
     public GameObject ScoreValue;
-    public TextMeshProUGUI timeOut;
+    // public TextMeshProUGUI timeOut;
+    public TextMeshProUGUI targetText;
     public int score;
     public int antCount;
     public int prevCount;
-   // public int timeLeft = 100;
-   // public int lim_;
-    //public bool set = false;
+    public int targetLeft;
+
+    // adding the event in the awake method
+    // then calling a subroutine based on that
+    private void Awake()
+    {
+        PinchDestroy.AntKilled += RunCo;
+        // runco is run coroutine
+
+    }
+
+    // define the co-routine
+    public void RunCo()
+    {
+        StartCoroutine(Pulse());
+    }
+
+    // define the co-routine Pulse
+    private IEnumerator Pulse()
+    {
+        for (float i = 1f; i <= 1.2f; i += 0.05f)
+        {
+            text.rectTransform.localScale = new Vector3(i, i, i);
+            targetText.rectTransform.localScale = new Vector3(i, i, i);
+            yield return new WaitForEndOfFrame();
+        }
+
+        text.rectTransform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        targetText.rectTransform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+
+        for (float i = 1.2f; i >= 1f; i -= 0.05f)
+        {
+            text.rectTransform.localScale = new Vector3(i, i, i);
+            targetText.rectTransform.localScale = new Vector3(i, i, i);
+            yield return new WaitForEndOfFrame();
+        }
+ 
+        text.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+        targetText.rectTransform.localScale = new Vector3(1f, 1f, 1f);
+
+        
+    }
+
+    // define the onDestroy functionality
+    private void OnDestroy()
+    {
+        PinchDestroy.AntKilled -= RunCo;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,61 +70,29 @@ public class ScoreManager : MonoBehaviour
         {
             instance = this;
         }
-        //prevCount = InstantiatePrefab.instance.returnCount();
-        //DecreaseTime();
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        /*antCount = InstantiatePrefab.instance.returnCount();
-        lim_ = InstantiatePrefab.instance.returnLim();
-        if (set == false)
-        {
-            DecreaseTime(); 
 
-            if (antCount >= prevCount) {
-
-                set = false;
-            }
-            else
-            {
-                set = true;
-            }
-            
-        }*/
 
     }
     public void ChangeScore(int AntCount)
     {
         score += AntCount;
-        text.text = "Ant Count : " + score.ToString();
+        text.text = score.ToString();
     }
-    /*public IEnumerator DecreaseTime()
+
+    public void TargetUpdate()
     {
+        // the target get's updated once an ant is killed
+        int killed = UpdateScore.instance.returnKills();
+        int target = InstantiatePrefab.instance.returnTarget();
+        targetLeft = target - killed;
+        targetText.text = targetLeft.ToString();
         
 
-        while (timeLeft > 0)
-        {
-            if (antCount >= prevCount)
-            {
-                yield return new WaitForSeconds(0.3f);
-                timeOut.text = "AntFrenzy In: " + timeLeft.ToString() + "s ";
-                timeLeft -= 1;
-                prevCount = antCount;
-            }
-            else if(antCount < prevCount)
-            {
-                timeLeft += 10;
-                prevCount = antCount;
-            }
-        }
-        if(timeLeft == 0)
-        {
-            PlayPause.instance.GameOver();
-        }
-        
-        
-
-    }*/
+    }
 }
