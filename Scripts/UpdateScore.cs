4e407aa401d7c989ad9a39f2ffc0209162f27b82
Siddharths8212376 <40669232+Siddharths8212376@users.noreply.github.com>
2019-12-27 22:20:58 +0530

    Resolved the progress bar bug...
    The progress is now correctly shown in the progress bar....

--- Scripts/UpdateScore.cs
+++ Scripts/UpdateScore.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
 
 public class UpdateScore : MonoBehaviour
 {
@@ -8,6 +10,10 @@ public class UpdateScore : MonoBehaviour
     public SimpleHealthBar healthBar;
     public static UpdateScore instance;
     // Start is called before the first frame update
+
+    // create the game object for specifying the progress
+    public SimpleHealthBar progressBar;
+
     public int kills = 1;
     void Start()
     {
@@ -24,12 +30,17 @@ public class UpdateScore : MonoBehaviour
     }
     public void UpdateHealth()
     {
-        healthBar.UpdateBar( (float)kills, (float)InstantiatePrefab.instance.returnLim());
+        // we should add the current time as well as update the progressbar
+        float up_time = InstantiatePrefab.instance.currentTime;
+        up_time += 1f;
+
+        healthBar.UpdateBar( up_time, InstantiatePrefab.instance.startingTime);
+        progressBar.UpdateBar((float)kills, (float)InstantiatePrefab.instance.antTarget);
         kills += 1;
-        if(kills > InstantiatePrefab.instance.returnLim())
+        /*if(kills > InstantiatePrefab.instance.returnTarget())
         {
             InstantiatePrefab.instance.WinLevel();
-        }
+        }*/
     }
     public int returnKills()
     {
